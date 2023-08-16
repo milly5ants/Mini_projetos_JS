6bc13c156986af4bba70f5d73974033ac31fd5ba
milly5ants <jamilyyy4@gmail.com>
2023-08-15 22:17:14 -0300

    Function Excluir Cliente e fechar modal ao salvar

--- projeto_CRUD/main.js
+++ projeto_CRUD/main.js
@@ -1,33 +1,31 @@
-'use strict'
+function adicionarCliente() {
+    const cliente = {
+        nome: document.getElementById('nomeCliente').value,
+        email: document.getElementById('emailCliente').value,
+        celular: document.getElementById('telefoneCliente').value,
+        cidade: document.getElementById('cidadeCliente').value
+    };
 
-//Interação com o layout
+    const index = document.getElementById('nomeCliente').dataset.index;
 
-const cliente = {
-    nome: document.getElementById('nomeCliente').value,
-    email: document.getElementById('emailCliente').value,
-    celular: document.getElementById('telefoneCliente').value,
-    cidade: document.getElementById('cidadeCliente').value
-}
-const index = document.getElementById('nomeCliente').dataset.index
-
-    function adicionarCliente (){
-            if (cliente.nome && cliente.email && cliente.celular && cliente.cidade) {
-                const newRow = document.createElement('tr');
-                newRow.innerHTML = `
-                    <td>${cliente.nome}</td>
-                    <td>${cliente.email}</td>
-                    <td>${cliente.celular}</td>
-                    <td>${cliente.cidade}</td>
-                    <td>
-                        <button type="button" id="editar-${index}">Editar</button>
-                        <button type="button" id="deletar-${index}" >Excluir</button>
-                    </td>
-                `;
-                document.querySelector('table>tbody').appendChild(newRow);
-            } 
-
-          
+    if (cliente.nome && cliente.email && cliente.celular && cliente.cidade) {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td>${cliente.nome}</td>
+            <td>${cliente.email}</td>
+            <td>${cliente.celular}</td>
+            <td>${cliente.cidade}</td>
+            <td>
+                <button type="button" id="editar-${index}" onclick="editarCliente(${index})">Editar</button>
+                <button type="button" id="deletar-${index}" onclick="excluirCliente(${index})">Excluir</button>
+            </td>
+        `;
+        document.querySelector('table>tbody').appendChild(newRow);
     }
-    
+}
 
+function excluirCliente(index) {
+    const excluirRegistroCliente = document.getElementById(`deletar-${index}`).closest('tr');
+    excluirRegistroCliente.remove();
+}
 
